bd51a1a1c5544f634f246635079ce98556d3dc46
Elias Lima Leite <limaleiteelias99@gmail.com>
2025-07-22 15:21:40 -0300

    Criando o Hero e importando font Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles(){
     return gulp.src('./src/styles/*.scss')
@@ -7,8 +8,14 @@ function styles(){
         .pipe(gulp.dest('./dist/styles/'));
 }
 
-
+function images(){
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images/'));
+}
 
 exports.default = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
 };
+
+exports.build = gulp.parallel(styles, images);
